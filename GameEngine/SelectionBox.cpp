c236410bf65d50017826947eda27eef2e97b65a9
Kurt D <kurtd5105@gmail.com>
2016-02-07 17:16:50 -0800

    Implement selection boxes object.

--- GameEngine/SelectionBox.cpp
+++ GameEngine/SelectionBox.cpp
@@ -7,4 +7,66 @@ namespace GameEngine {
 
 	SelectionBox::~SelectionBox() {
 	}
+
+	void SelectionBox::init(float buttonX, float y, float buttonWidth, float buttonHeight, float textWidth, float depth, std::vector<std::string> options,
+							std::string nameLeft, std::string nameRight, std::string textLPath, std::string aniLPath, std::string textRPath, std::string aniRPath,
+							Color color, std::function<void(void)> callbackLeft, std::function<void(void)> callbackRight, SpriteManager* manager, FontBatcher* fontBatcher) {
+		_leftX = buttonX;
+		_rightX = buttonX + buttonWidth + textWidth;
+		_textX = buttonX + buttonWidth;
+		_y = y;
+		_buttonWidth = buttonWidth;
+		_textWidth = textWidth;
+		_depth = depth;
+		_options = options;
+		_count = _options.size();
+		_index = 0;
+
+		_SpriteManager = manager;
+		_FontBatcher = fontBatcher;
+
+		_left.init(_leftX, _y, _buttonWidth, buttonHeight, _depth, textLPath, aniLPath, nameLeft, callbackLeft, _SpriteManager);
+		_right.init(_rightX, _y, _buttonWidth, buttonHeight, _depth, textRPath, aniRPath, nameRight, callbackRight, _SpriteManager);
+
+		_color = color;
+		_text.init(_options[_index], glm::vec2(_textX, _y), glm::vec2(1, 1), _depth, _color, _FontBatcher);
+	}
+
+	void SelectionBox::setSelection(std::string selection) {
+		unsigned int index;
+		for(index = 0; index < _count; index++) {
+			if(_options[index] == selection) {
+				setSelection(index);
+				return;
+			}
+		}
+	}
+
+	void SelectionBox::setSelection(unsigned int index) {
+		_index = index;
+		_text.init(_options[_index], glm::vec2(_textX, _y), glm::vec2(1, 1), _depth, _color, _FontBatcher);
+	}
+
+	void SelectionBox::setOptions(std::vector<std::string> options) {
+		_options = options;
+		_count = _options.size();
+		_index = 0;
+	}
+
+	void SelectionBox::forward() {
+		_index++;
+		if(_index >= _count) {
+			_index = 0;
+		}
+		_text.init(_options[_index], glm::vec2(_textX, _y), glm::vec2(1, 1), _depth, _color, _FontBatcher);
+	}
+
+	void SelectionBox::backward() {
+		if(_index == 0) {
+			_index = _count - 1;
+		} else {
+			_index--;
+		}
+		_text.init(_options[_index], glm::vec2(_textX, _y), glm::vec2(1, 1), _depth, _color, _FontBatcher);
+	}
 }
--- GameEngine/SelectionBox.h
+++ GameEngine/SelectionBox.h
@@ -1,11 +1,48 @@
 #pragma once
 
-#include "Button.h"
+#include <functional>
+
+#include "FontBatcher.h"
+#include "Hitbox.h"
+#include "InputManager.h"
+#include "SimpleButton.h"
+#include "Sprite.h"
+#include "SpriteManager.h"
+#include "SpriteSheet.h"
+#include "Text.h"
+#include "Vertex.h"
 
 namespace GameEngine {
-	class SelectionBox : public Button {
+	class SelectionBox {
 	public:
 		SelectionBox();
 		~SelectionBox();
+
+		void init(float buttonX, float y, float buttonWidth, float buttonHeight, float textWidth, float depth, std::vector<std::string> options,
+				  std::string nameLeft, std::string nameRight, std::string textLPath, std::string aniLPath, std::string textRPath, std::string aniRPath,
+				  Color color, std::function<void(void)> callbackLeft, std::function<void(void)> callbackRight, SpriteManager* manager, FontBatcher* fontBatcher);
+
+		void setSelection(std::string selection);
+		void setSelection(unsigned int index);
+
+		void setOptions(std::vector<std::string> options);
+
+		void forward();
+		void backward();
+
+		SimpleButton* getLeft() { return &_left; }
+		SimpleButton* getRight() { return &_right; }
+
+	private:
+		float _leftX, _rightX, _textX, _y, _buttonWidth, _textWidth, _depth;
+		unsigned int _count, _index;
+		std::vector<std::string> _options;
+
+		SpriteManager* _SpriteManager;
+		FontBatcher* _FontBatcher;
+
+		SimpleButton _left, _right;
+		Text _text;
+		Color _color;
 	};
 }
