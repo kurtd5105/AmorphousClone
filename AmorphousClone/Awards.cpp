df8c3c4e78db103ad7ca52b61e6d0947d6279d2e
Kurt D <kurtd5105@gmail.com>
2016-05-02 22:15:40 -0700

    Add comments for Awards and a fix.

--- AmorphousClone/Awards.cpp
+++ AmorphousClone/Awards.cpp
@@ -17,13 +17,19 @@ void Awards::loadAwards(uint32_t a, uint32_t b, uint32_t c) {
 }
 
 std::string Awards::saveAwards() {
+	// Encrypt temporarily to save it to a string.
 	encrypt();
-	std::string awards = std::to_string(awards[0]) + std::to_string(awards[0]) + std::to_string(awards[0]);
+
+	// Create the string by combining the string.
+	std::string awards = std::to_string(awards[0]) + std::to_string(awards[1]) + std::to_string(awards[2]);
+
+	// Decrypt the awards again so they are unchanged after the function returns.
 	decrypt();
 	return awards;
 }
 
 void Awards::encrypt() {
+	// Perform a bitwise shift, bitwise negation, and then XOR with 0xBADB.
 	for(auto& i : awards) {
 		i <<= 4;
 		i = ~i;
@@ -32,6 +38,7 @@ void Awards::encrypt() {
 }
 
 void Awards::decrypt() {
+	// Reverse the encryption process by following the encryption process in reverse.
 	for(auto& i : awards) {
 		i ^= 0xBADB;
 		i = ~i;
--- AmorphousClone/Awards.h
+++ AmorphousClone/Awards.h
@@ -8,15 +8,16 @@ public:
 	Awards();
 	~Awards();
 
-	//Set up the awards class by taking the input numbers and decrypting them
+	// Set up the awards class by taking the input numbers (that are encrypted) and decrypting them.
 	void loadAwards(uint32_t a, uint32_t b, uint32_t c);
 
-	//Set the ith award (a = 0, b = 1, c = 2) flag (can set 1 bit or multiple) (awards[i] & flag)
+	// Set the ith award (a = 0, b = 1, c = 2) flag (can set 1 bit or multiple) (awards[i] & flag).
 	void setAward(unsigned int i, uint32_t flag) { awards[i] &= flag; }
 
+	// Get a given set of awards.
 	uint32_t getAward(unsigned int i) { return awards[i]; }
 
-	//Save the awards to a string by encrypting them
+	// Save the awards to a string by encrypting them. Must be called before outputting to save file.
 	std::string saveAwards();
 
 private:
